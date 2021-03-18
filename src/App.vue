a0b46af1633f01521e5f06be87dfebd128e17926
xinhaolin <linxinhao9704@163.com>
2021-03-18 23:08:58 +0800

    初步表单

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,41 @@
 <template>
-<div class="container" >
-  <global-header :user="currentUser" ></global-header>
-  <column-list :list="list" ></column-list>
-</div>
+  <div class="container">
+    <global-header :user="currentUser"></global-header>
+    <column-list v-if="false" :list="list"></column-list>
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          v-model="emailRef.val"
+          @blur="validateEmail"
+        />
+        <div v-if="emailRef.error" id="emailHelp" class="form-text">
+         {{emailRef.message}}
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
@@ -40,6 +69,7 @@ const testData: ColumnProps[] = [
     // avatar: 'http://img0.pconline.com.cn/pconline/news/it/2103/20213/16/16158785313313510.png'
   }
 ]
+const emailReg = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/
 export default defineComponent({
   name: 'App',
   components: {
@@ -47,14 +77,29 @@ export default defineComponent({
     GlobalHeader
   },
   setup () {
+    const emailRef = reactive({
+      val: '',
+      error: false,
+      message: ''
+    })
+    const validateEmail = () => {
+      if (emailRef.val.trim() === '') {
+        emailRef.error = true
+        emailRef.message = 'can not be empty'
+      } else if (!emailReg.test(emailRef.val)) {
+        emailRef.error = true
+        emailRef.message = 'should be valid email'
+      }
+    }
     return {
       list: testData,
-      currentUser
+      currentUser,
+      emailRef,
+      validateEmail
     }
   }
 })
 </script>
 
 <style>
-
 </style>
