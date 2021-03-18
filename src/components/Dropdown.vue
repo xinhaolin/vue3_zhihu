63bcdfc57538bf902cae6f3baac00ece6efcfe69
xinhaolin <linxinhao9704@163.com>
2021-03-18 22:53:02 +0800

    header组件完成

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,30 +1,38 @@
 <template>
-  <div class="dropdown" >
-    <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" >{{title}}</a>
-    <ul class="dropdown-menu" >
-      <li class=" dropdown-item" >
-        <a href="#">新建文章</a>
-      </li>
-      <li class=" dropdown-item" >
-        <a href="#">编辑资料</a>
-      </li>
+  <div class="dropdown" ref="dropdownRef" >
+    <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen" >{{title}}</a>
+    <ul class="dropdown-menu" :style="{display:'block'}" v-if="isOpen" >
+      <slot></slot>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref, watch } from 'vue'
+import useClickOutside from '../hooks/useClickOutside'
 
 export default defineComponent({
   name: 'Dropdown',
   props: {
     title: {
       type: String,
-      defaulted: true
+      default: ''
     }
   },
   setup () {
-    return {}
+    const isOpen = ref(false)
+
+    const dropdownRef = ref<null | HTMLElement>(null)
+    const toggleOpen = () => {
+      isOpen.value = !isOpen.value
+    }
+    const isClickOutside = useClickOutside(dropdownRef)
+    watch(isClickOutside, () => {
+      if (isOpen.value && isClickOutside.value) {
+        isOpen.value = false
+      }
+    })
+    return { isOpen, toggleOpen, dropdownRef }
   }
 })
 </script>
--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -6,13 +6,22 @@
        <li class="list-inline-item" ><a href="#" class="btn btn-outline-light my-2" >注册</a></li>
      </ul>
      <ul v-else class=" list-inline mb-0" >
-       <li class="list-inline-item" ><a href="#" class="btn btn-outline-light my-2" >你好 {{user.name}}</a></li>
+       <li class="list-inline-item" >
+         <drop-down :title="`你好，${user.name}`" >
+           <dropdown-item><a href="#" class=" dropdown-item" >新建文章</a> </dropdown-item>
+           <dropdown-item disabled ><a href="#" class=" dropdown-item"   >编辑资料</a> </dropdown-item>
+           <dropdown-item><a href="#" class=" dropdown-item" >退出登录</a> </dropdown-item>
+         </drop-down>
+         <!-- <a href="#" class="btn btn-outline-light my-2" >你好 {{user.name}}</a> -->
+        </li>
      </ul>
   </nav>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
+import DropDown from './Dropdown.vue'
+import DropdownItem from './DropdownItem.vue'
 export interface UserProps{
   isLogin: boolean;
   name?: string;
@@ -25,6 +34,10 @@ export default defineComponent({
       type: Object as PropType<UserProps>,
       required: true
     }
+  },
+  components: {
+    DropDown,
+    DropdownItem
   },
   setup () {
     return {}
